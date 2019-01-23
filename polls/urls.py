82377fff2a2491eae911bfb15b347290d3d5cdf1
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2019-01-22 18:33:35 -0700

    api

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # ex: /polls/5/
     path('savenew/', views.SaveNew, name='saveNew'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,17 @@
+import json
 from django.http import JsonResponse
 from django.http import HttpResponse
 from .models import Question
 from .models import QuestionForm
 from django.views.decorators.csrf import csrf_exempt
 
-def index(request):
+def index(request, format=None):
     polls = Question.objects.all()[:20]
     data = {"results": list(polls.values("question_text", "pub_date"))}
     return JsonResponse(data)
 
 @csrf_exempt
-def SaveNew(request):
+def SaveNew(request, format=None):
     if request.method == "POST":
         form = QuestionForm(request.POST)
         if form.is_valid():
@@ -20,9 +21,19 @@ def SaveNew(request):
             return JsonResponse({'message': 'failed'})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+@csrf_exempt
+def results(request):
+    if 'application/json' in request.META['CONTENT_TYPE']:
+        data = json.loads(request.body)
+        question = data.get('question_text', None)
+        pub_time = data.get('pub_date', None)
+
+        q = Question(question_text=question, pub_date=pub_time)
+        q.save()
+        return JsonResponse({'message': 'saved'})
+    else:
+        return JsonResponse({'message': 'failed'})
+
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
